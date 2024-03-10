18312adc873f6fd197d08282d2fffbd41fc37df5
jericho-0929 <benitez.jericho0929@gmail.com>
2024-03-11 02:14:31 +0800

    No message

--- app/src/main/java/com/example/thesisocr/ImageProcessing.kt
+++ app/src/main/java/com/example/thesisocr/ImageProcessing.kt
@@ -2,7 +2,6 @@ package com.example.thesisocr
 
 import android.graphics.Bitmap
 import org.opencv.android.Utils
-import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
@@ -11,8 +10,8 @@ class ImageProcessing {
     fun processImageForRecognition(inputBitmap: Bitmap): Bitmap {
         val grayMat = convertToGrayscaleMat(inputBitmap)
         val equalizedMat = histogramEqualization(grayMat)
-        val blurredMat = imageBlur(equalizedMat)
-        val thresholdMat = imageThresholding(blurredMat)
+        val blurredMat = gaussianBlur(equalizedMat)
+        val thresholdMat = blurredMat
         return convertToBitmap(thresholdMat)
     }
     private fun convertToGrayscaleMat(inputBitmap: Bitmap): Mat {
@@ -23,26 +22,25 @@ class ImageProcessing {
         return grayMat
     }
     private fun histogramEqualization(inputMat: Mat): Mat {
-        // Use CLAHE
         val equalizedMat = Mat()
-        Imgproc.createCLAHE(2.0, Size(8.0, 8.0)).apply(inputMat, equalizedMat)
+        Imgproc.equalizeHist(inputMat, equalizedMat)
         return equalizedMat
     }
-    private fun imageBlur(inputMat: Mat): Mat {
+    private fun gaussianBlur(inputMat: Mat): Mat {
         val blurredMat = Mat()
-        Imgproc.GaussianBlur(inputMat, blurredMat, Size(5.0, 5.0), 0.0)
+        Imgproc.medianBlur(inputMat, blurredMat, 3)
         return blurredMat
     }
     private fun imageThresholding(inputMat: Mat): Mat {
         val thresholdMat = Mat()
-        Imgproc.threshold(inputMat, thresholdMat, 135.0, 255.0, Imgproc.THRESH_OTSU)
+        Imgproc.threshold(inputMat, thresholdMat, 0.0, 255.0, Imgproc.THRESH_OTSU)
         return thresholdMat
     }
-    private fun dilation(inputMat: Mat): Mat {
-        val dilatedMat = Mat()
-        val kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(2.0, 2.0))
-        Imgproc.dilate(inputMat, dilatedMat, kernel)
-        return dilatedMat
+    private fun openingOperation(inputMat: Mat): Mat {
+        val morphMat = Mat()
+        val morphKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(5.0, 5.0))
+        Imgproc.morphologyEx(inputMat, morphMat, Imgproc.MORPH_OPEN, morphKernel)
+        return morphMat
     }
     private fun convertToBitmap(inputMat: Mat): Bitmap {
         val outputBitmap = Bitmap.createBitmap(inputMat.width(), inputMat.height(), Bitmap.Config.ARGB_8888)
